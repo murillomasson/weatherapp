9c72405d9d22979426e6c5dd456004ae781cded4
Murillo Masson Guapo <97128702+murillomasson@users.noreply.github.com>
2022-12-01 06:06:52 -0300

    Update urls.py

--- openWeatherMap/app/urls.py
+++ openWeatherMap/app/urls.py
@@ -1,6 +1,17 @@
+from django.contrib import admin
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+
+    # User Authentication
+    path('registeruser/', views.registeruser, name="register_user"),
+    path('loginuser/', views.loginuser, name="login_user"),
+    path('logoutuser/', views.logoutuser, name="logout_user"),
+
+    # Use login main window
+    path('delete/<name>/', views.delete, name='delete'),
+    path('main/', views.mainpage, name="main"),
+
 ]
